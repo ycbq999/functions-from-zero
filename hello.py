8e0a39db6ce5de5f172351fd4a0b2b69be3a7354
STC <ycbq999@hotmail.com>
2023-07-25 07:26:09 +0000

    finalizing statements using copilot:

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-x = 1
-y = 2
-import ipdb
+# create a list with 10 different types of fruit
+fruit = [
+    "apple",
+    "banana",
+    "cherry",
+    "orange",
+    "kiwi",
+    "melon",
+    "mango",
+    "pear",
+    "pineapple",
+    "grape",
+]
+# loop through the fruits and count the total fruits in the list
+for i in fruit:
+    # use an f-string to print the fruit name and the number of the fruit in the list
+    print(f"{i} is fruit number {fruit.index(i) + 1} in the list")
 
-ipdb.set_trace()
-print(x + y)
+# the total number of fruits in the list
+print(f"There are {len(fruit)} fruits in the list")
