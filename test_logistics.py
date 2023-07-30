48ba53184199d5aa9217ed8f419c84a73ee3e92e
STC <ycbq999@hotmail.com>
2023-07-30 04:50:17 +0000

    adding logic for tests for fastapi

--- test_logistics.py
+++ test_logistics.py
@@ -1,5 +1,8 @@
 from mylib.logistics import distance_between_two_points, cities_list, CITIES
+from fastapi.testclient import TestClient
 
+from main import app
+import pytest
 
 def test_distance_between_two_points():
     """
@@ -13,3 +16,14 @@ def test_cities_list():
     This function tests the print_cities function
     """
     assert "New York" in cities_list()
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        yield client
+
+def test_read_main(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Logistics INC"}
